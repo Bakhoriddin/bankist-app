6ea053ef9a531f9166bfd02dfb5759cdacd69232
Bakhoriddin <faybahor@gmail.com>
2022-11-20 03:19:16 +0500

    Added loan functionality

--- script.js
+++ script.js
@@ -171,6 +171,22 @@ btnTransfer.addEventListener('click', function(e) {
   }
 });
 
+// Loan functionality
+btnLoan.addEventListener('click', function(e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+
+  if(
+    amount > 0 && 
+    currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+      // Add movement
+      currentAccount.movements.push(amount);
+      // Update UI
+      updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
 // Account Close functionality
 btnClose.addEventListener(function(e) {
   e.preventDefault();
