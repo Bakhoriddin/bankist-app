49a4d323a60f1261fddaed45b8af001e52ed223c
Bakhoriddin <faybahor@gmail.com>
2022-11-20 00:36:47 +0500

    Added delete account functionality

--- script.js
+++ script.js
@@ -170,3 +170,20 @@ btnTransfer.addEventListener('click', function(e) {
       updateUI(currentAccount);
   }
 });
+
+// Account Close functionality
+btnClose.addEventListener(function(e) {
+  e.preventDefault();
+  if(
+    inputCloseUsername.value === currentAccount.username && 
+    Number(inputClosePin.value) === currentAccount.pin) {
+      const index = accounts.findIndex(
+        acc => acc.username === currentAccount.username);
+        // Delete account
+      accounts.splice(index, 1);
+      // Hide UI
+      containerApp.style.opacity = 0;
+  }
+  inputClosePin.value = inputCloseUsername.value = '';
+  labelWelcome.textContent = 'Log in to get started';
+});
